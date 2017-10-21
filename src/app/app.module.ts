c4387b70b0f15ec5861ad0be2fbf5d497d7d213d
Prashant Dhavale <pndhavale@outlook.com>
2017-10-21 11:28:37 +1100

    Integrate Material Design for angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule, 
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
